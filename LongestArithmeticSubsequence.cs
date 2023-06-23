6349c2c43e8deae1b3ba8b7b6a515d98425e5c07
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-23 16:02:36 +0300

    Добавьте файлы проекта.

---
+++ LongestArithmeticSubsequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestArithmeticSubsequence
+{
+	internal class LongestArithmeticSubsequence
+	{
+		public int LongestArithSeqLength(int[] nums)
+		{
+			int n = nums.Length;
+			int longestArithmeticSuseqLength = 0;
+			Dictionary<int, int>[] dp = new Dictionary<int, int>[n];
+			for (int i = 0; i < n; ++i)
+			{
+				dp[i] = new Dictionary<int, int>();
+				for (int j = 0; j < i; ++j)
+				{
+					int difference = nums[i] - nums[j];
+					dp[i][difference] = dp[j].GetValueOrDefault(difference, 1) + 1;
+					longestArithmeticSuseqLength = Math.Max(longestArithmeticSuseqLength, dp[i][difference]);
+				}
+			}
+			return longestArithmeticSuseqLength;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LongestArithmeticSubsequence
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			LongestArithmeticSubsequence longestArithmeticSubsequence = new();
+            Console.WriteLine(longestArithmeticSubsequence.LongestArithSeqLength(new int[] { 3, 6, 9, 12 }));
+			Console.WriteLine(longestArithmeticSubsequence.LongestArithSeqLength(new int[] { 9, 4, 7, 2, 10 }));
+			Console.WriteLine(longestArithmeticSubsequence.LongestArithSeqLength(new int[] { 20, 1, 15, 3, 10, 5, 8 }));
+		}
+	}
+}
